8bb8748d19e3d50211233c055459fac2ea0b2580
Nelly Sugu <jonly03@gmail.com>
2020-09-23 11:20:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { level1, level2, level3 } = require("./questions");
+const express = require("express");
+
+const server = express();
+
+server.get("/:level", (req, res) => {
+  const { level } = req.params;
+  let questions = [];
+
+  if (level === "level1") {
+    questions = level1;
+  } else if (level === "level2") {
+    questions = level2;
+  } else if (level === "level3") {
+    questions = level3;
+  } else {
+    return res.json({
+      error: "questions only exists in /level1, /level2, or '/level3",
+    });
+  }
+  const randIdx = Math.floor(Math.random() * questions.length);
+  return res.json({
+    q: questions[randIdx],
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("server listening");
+});
